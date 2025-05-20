f444e68c58ad1645d17e2aa84e38aff90b3ab627
Jada Baron <jadachantellebaron@gmail.com>
2025-05-20 18:32:02 +0100

    Put together calc functions to solve challenges
    Used reduce array method for function returns that need to be a single num.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,67 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+console.log("add", add(5, 10));
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+console.log("subtract", subtract(100, 10));
+
+const sum = function (arr) {
+  let total = arr.reduce((accum, num) => {
+    accum += num;
+    return accum;
+  }, 0);
+
+  return total;
 };
 
-const multiply = function() {
+console.log("sum", sum([3, 4, 13]));
 
+const multiply = function (arr) {
+  for (let i = 0; i <= arr.length; i++) {
+    let total = arr.reduce((accum, num) => {
+      accum = accum * num;
+      return accum;
+    });
+    return total;
+  }
 };
 
-const power = function() {
-	
+console.log("multiply", multiply([2, 4, 10]));
+
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+console.log("power", power(4, 3));
+
+const factorial = function (num) {
+  let numArr = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (!numArr) {
+      numArr.push(1);
+    }
+    numArr.push(i);
+  }
+
+  return multiplyNumArrItems(numArr);
+
+  function multiplyNumArrItems(arr) {
+    let factorialNum = arr.reduce((accum, arrItem) => {
+      accum = accum * arrItem;
+      return accum;
+    });
+
+    return factorialNum;
+  }
 };
+
+console.log("factorial", factorial(5));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +70,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
